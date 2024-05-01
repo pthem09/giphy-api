395028de9a583e032ceab0ff86172fe75b955ce6
Paul Them <->
2024-04-30 23:04:41 -0400

    edited js file; need to debug API key

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
-const API_PREFIX_GIFS = "api.giphy.com/v1/gifs/search?api-key=";
-const API_PREFIX_STICKERS = "api.giphy.com/v1/stickers/search?api-key=";
+const API_PREFIX_GIFS = "https://api.giphy.com/v1/gifs/search?api-key=";
+const API_PREFIX_STICKERS = "https://api.giphy.com/v1/stickers/search?api-key=";
 const API_KEY = "734eYDXm24Vsbjq8iOtzC0EA1fW9XTyF";
-const API_SETTINGS = "limit=50&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+const API_SETTINGS = ""; //"&limit=50&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
 function getPrompt(event) {
     event.preventDefault();
     let userSearch = document.querySelector("[name=memeSearch]").value;
-    window.alert(userSearch);
+    fetchMemes(userSearch);
+}
+
+function fetchMemes(textInput) {
+    console.log(`${API_PREFIX_GIFS}${API_KEY}&q=${textInput}${API_SETTINGS}`);
+    
+    fetch(`${API_PREFIX_GIFS}${API_KEY}&q=${textInput}${API_SETTINGS}`)
+        .then((data) => data.json())
+        .then(returnImgs)
+        .catch(() => alertUser("Content unavailable."));
+}
+
+function returnImgs(apiText) {
+    let htmlFormat = "";
+
+    if (apiText.length === 0 ){
+        alertUser("No results -- try another search.");
+    } else {
+        for (let meme of apiText.data){
+            htmlFormat += `
+                <img src="${meme.images.original.url}" alt="${meme.alt_text}" class="meme-sticker" />
+            `;
+        }
+    }
+
+    document.querySelector(".js-display-memes").innerHTML = apiText;
+
+}
+
+function alertUser(errText) {
+    document.querySelector(".js-display-memes").innerHTML = `<div class="alert alert-warning">${errText}</div>`;
 }
 
 document.querySelector("#memeForm").addEventListener("submit", getPrompt);
