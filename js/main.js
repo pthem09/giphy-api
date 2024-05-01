033e4fc38d88f47c4401c0dc305670575ffb9a68
Paul Them <->
2024-04-30 22:25:49 -0400

    changing meme form and input variables

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+const API_PREFIX_GIFS = "api.giphy.com/v1/gifs/search?api-key=";
+const API_PREFIX_STICKERS = "api.giphy.com/v1/stickers/search?api-key=";
 const API_KEY = "734eYDXm24Vsbjq8iOtzC0EA1fW9XTyF";
+const API_SETTINGS = "limit=50&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
+function getPrompt(event) {
+    event.preventDefault();
+    let userSearch = document.querySelector("[name=memeSearch]").value;
+    window.alert(userSearch);
+}
 
-//javascript, jQuery
+document.querySelector("#memeForm").addEventListener("submit", getPrompt);
